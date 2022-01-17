d09e7f69573e46551be9b7cf2bef91664d68102d
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-01-17 12:44:32 -0300

    Configurando
    Foi instalado as dependencias de desenvolvimento e foi configurado o next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase) => {
+	let env = {}
+
+	if (phase === PHASE_DEVELOPMENT_SERVER) {
+		env = {
+			customKey: 'development'
+		}
+	} else {
+		env = {
+			customKey: 'production'
+		}
+	}
+
+	return {
+		trailingSlash: true,
+		reactStrictMode: true,
+		env
+	}
 }
